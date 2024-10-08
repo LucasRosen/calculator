ed54776f9aee5f13cf7b31b1c5f1d64e6a6f805c
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-10-08 18:49:27 +0200

    Add query selectors and and event listeners for all buttons

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// UI
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+
+const equalsButton = document.getElementById("equals");
+const clearEntryButton = document.getElementById("clear-entry");
+const clearButton = document.getElementById("clear");
+const delButton = document.getElementById("delete");
+const plusMinusButton = document.getElementById("plus-minus")
+const commaButton = document.getElementById("comma");
+
+equalsButton.addEventListener("click", evaluate)
+clearEntryButton.addEventListener("click", clearEntry);
+clearButton.addEventListener("click", clear);
+delButton.addEventListener("click", deleteNumber);
+plusMinusButton.addEventListener("click", plusOrMinus);
+commaButton.addEventListener("click", appendComma);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", updateNumber)
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", updateOperator)
+);
+
 function operate(operator, a, b) {
   switch (operator) {
     case "add":
@@ -9,4 +35,10 @@ function operate(operator, a, b) {
     case "divide":
       return a / b;
   }
+}
+
+function updateNumber(event) {
+}
+
+function updateOperator(event) {
 }
